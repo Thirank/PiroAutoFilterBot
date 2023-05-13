f8c9b29db5e7e604b83bc6e59c7d172a486ab3b6
Thirank <116568300+Thirank@users.noreply.github.com>
2023-05-13 22:31:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,15 +22,23 @@ BATCH_FILES = {}
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
-                    InlineKeyboardButton('➕ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+                    InlineKeyboardButton('≈ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ≈', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+
                 ],[
-                    InlineKeyboardButton('🛡 𝖮𝗐𝗇𝖾𝗋', callback_data="owner_info"),
+
+                    InlineKeyboardButton('👨‍🎓 Oᴡɴᴇʀ', callback_data="owner_info"),
+
                     InlineKeyboardButton('🧩 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖦𝗋𝗈𝗎𝗉', url=f"https://t.me/{SUPPORT_CHAT}")
+
                 ],[
-                    InlineKeyboardButton('ℹ️ 𝖧𝖾𝗅𝗉', callback_data='help'),
-                    InlineKeyboardButton('😊 𝖠𝖻𝗈𝗎𝗍', callback_data='about')
+
+                    InlineKeyboardButton('💠 Hᴇʟᴘ 💠', callback_data='help'),
+
+                    InlineKeyboardButton('♻️ Aʙᴏᴜᴛ ♻️', callback_data='about'),
+
                 ],[
-                    InlineKeyboardButton('🔎 𝖨𝗇𝗅𝗂𝗇𝖾 𝖲𝖾𝖺𝗋𝖼𝗁', switch_inline_query_current_chat='')
+
+                    InlineKeyboardButton('🎭 Mᴄ Hᴅ Oғғɪᴄɪᴀʟ 🎭', url=f"https://t.me/Mc_Film_Mainchannel")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
@@ -45,15 +53,23 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('➕ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+                    InlineKeyboardButton('≈ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ≈', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+
                 ],[
-                    InlineKeyboardButton('🛡 𝖮𝗐𝗇𝖾𝗋', callback_data="owner_info"),
+
+                    InlineKeyboardButton('👨‍🎓 Oᴡɴᴇʀ', callback_data="owner_info"),
+
                     InlineKeyboardButton('🧩 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖦𝗋𝗈𝗎𝗉', url=f"https://t.me/{SUPPORT_CHAT}")
+
                 ],[
-                    InlineKeyboardButton('ℹ️ 𝖧𝖾𝗅𝗉', callback_data='help'),
-                    InlineKeyboardButton('😊 𝖠𝖻𝗈𝗎𝗍', callback_data='about'),
+
+                    InlineKeyboardButton('💠 Hᴇʟᴘ 💠', callback_data='help'),
+
+                    InlineKeyboardButton('♻️ Aʙᴏᴜᴛ ♻️', callback_data='about'),
+
                 ],[
-                    InlineKeyboardButton('🔎 𝖨𝗇𝗅𝗂𝗇𝖾 𝖲𝖾𝖺𝗋𝖼𝗁', switch_inline_query_current_chat='')
+
+                    InlineKeyboardButton('🎭 Mᴄ Hᴅ Oғғɪᴄɪᴀʟ 🎭', url=f"https://t.me/Mc_Film_Mainchannel")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -93,15 +109,23 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('➕ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+                    InlineKeyboardButton('≈ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ≈', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+
                 ],[
-                    InlineKeyboardButton('🛡 𝖮𝗐𝗇𝖾𝗋', callback_data="owner_info"),
+
+                    InlineKeyboardButton('👨‍🎓 Oᴡɴᴇʀ', callback_data="owner_info"),
+
                     InlineKeyboardButton('🧩 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖦𝗋𝗈𝗎𝗉', url=f"https://t.me/{SUPPORT_CHAT}")
+
                 ],[
-                    InlineKeyboardButton('ℹ️ 𝖧𝖾𝗅𝗉', callback_data='help'),
-                    InlineKeyboardButton('😊 𝖠𝖻𝗈𝗎𝗍', callback_data='about')
+
+                    InlineKeyboardButton('💠 Hᴇʟᴘ 💠', callback_data='help'),
+
+                    InlineKeyboardButton('♻️ Aʙᴏᴜᴛ ♻️', callback_data='about'),
+
                 ],[
-                    InlineKeyboardButton('🔎 𝖨𝗇𝗅𝗂𝗇𝖾 𝖲𝖾𝖺𝗋𝖼𝗁', switch_inline_query_current_chat='')
+
+                    InlineKeyboardButton('🎭 Mᴄ Hᴅ Oғғɪᴄɪᴀʟ 🎭', url=f"https://t.me/Mc_Film_Mainchannel")
                     
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
